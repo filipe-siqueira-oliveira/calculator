5c7825f9ce567135451d54c19087065a8c3107cc
Filipe Siqueira <fcsiqueira1409@gmail.com>
2023-04-06 12:01:53 -0300

    Finalização e responsividade

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,32 @@
+document.getElementById("toggle").addEventListener("click", () => {
+  document.getElementById("theme").classList.toggle("dark");
+  document.getElementById("theme").classList.toggle("light");
+  document.getElementById("display").classList.toggle("displayDark");
+  document.getElementById("display").classList.toggle("displayLight");
+  document.getElementById("switcher").classList.toggle("sliderLight");
+  document.getElementById("switcher").classList.toggle("sliderDark");
+});
+
+console.log(document.getElementById("theme").classList.value);
+
+let result = "";
+let display = "";
+
 function setNumber(number) {
+  if (result == document.getElementById("display").value) {
+    document.getElementById("display").value = "";
+    result = "";
+  }
   document.getElementById("display").value += number;
 }
 
 function setOperator(op) {
-  document.getElementById("display").value += op;
+  display = document.getElementById("display").value;
+  if (display == "") {
+    document.getElementById("display").value = "";
+  } else {
+    document.getElementById("display").value += op;
+  }
 }
 
 function setAllClear() {
@@ -11,8 +34,32 @@ function setAllClear() {
 }
 
 function calculate() {
-  let display = document.getElementById("display").value;
-  let result = eval(display.toString());
+  display = document.getElementById("display").value;
+  result = eval(display.toString());
 
   document.getElementById("display").value = result;
+}
+
+function setPercentage() {
+  display = document.getElementById("display").value;
+  result = eval(display.toString()) / 100;
+
+  document.getElementById("display").value = result;
+}
+
+function setPoint(number) {
+  display = document.getElementById("display").value;
+  if (result == document.getElementById("display").value) {
+    if (display.length == "") {
+      document.getElementById("display").value = "";
+      result = "";
+      document.getElementById("display").value += 0 + number;
+    } else {
+      document.getElementById("display").value = "";
+      result = "";
+      document.getElementById("display").value += 0 + number;
+    }
+  } else {
+    document.getElementById("display").value += number;
+  }
 }
